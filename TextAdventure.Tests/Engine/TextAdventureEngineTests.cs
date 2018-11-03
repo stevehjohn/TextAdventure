36a734f149de3e83b13c105759b492a1e422a9fd
Stevö John <stevehuwjohn@gmail.com>
2018-11-03 10:37:56 +0000

    More tests

--- TextAdventure.Tests/Engine/TextAdventureEngineTests.cs
+++ TextAdventure.Tests/Engine/TextAdventureEngineTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TextAdventure.Engine;
 using TextAdventure.Infrastructure;
 using TextAdventure.Models;
@@ -12,14 +13,16 @@ namespace TextAdventure.Tests.Engine
     public class TextAdventureEngineTests
     {
         private Mock<IInputOutput> _inputOutput;
+        private List<Location> _map;
         private TextAdventureEngine _engine;
 
         [SetUp]
         public void SetUp()
         {
             _inputOutput = new Mock<IInputOutput>();
+            _map = GetBasicMap();
 
-            _engine = new TextAdventureEngine(_inputOutput.Object, GetBasicMap());
+            _engine = new TextAdventureEngine(_inputOutput.Object, _map);
         }
 
         [TestCase("go north", "North")]
@@ -28,6 +31,7 @@ namespace TextAdventure.Tests.Engine
         [TestCase("go west", "West")]
         [TestCase("move north", "North")]
         [TestCase("head north", "North")]
+        [TestCase("go", "Go where exactly?\n")]
         public void Interprets_move_instructions_correctly(string instruction, string expected)
         {
             var count = 0;
@@ -44,6 +48,33 @@ namespace TextAdventure.Tests.Engine
             _inputOutput.Verify(io => io.Write(It.IsAny<ConsoleColor>(), expected, true));
         }
 
+        [Test]
+        public void Pickup_and_drop_items_correctly()
+        {
+            var count = 0;
+
+            _inputOutput.Setup(io => io.Input())
+                        .Returns(() =>
+                        {
+                            count++;
+                            switch (count)
+                            {
+                                case 1:
+                                    return "take axe";
+                                case 2:
+                                    return "go north";
+                                case 3:
+                                    return "drop axe";
+                                default:
+                                    return "bye";
+                            }
+                        });
+
+            _engine.RunGame();
+
+            Assert.True(_map.First(l => l.Coords.X == 0 && l.Coords.Y == 1).Items.Any(i => i.Description == "Axe"));
+        }
+
         private static List<Location> GetBasicMap()
         {
             return new List<Location>
@@ -51,7 +82,14 @@ namespace TextAdventure.Tests.Engine
                 new Location
                 {
                     Coords = new Coords(0, 0),
-                    Description = "Start"
+                    Description = "Start",
+                    Items = new List<Item>
+                    {
+                        new Item
+                        {
+                            Description = "Axe"
+                        }
+                    }
                 },
                 new Location
                 {
